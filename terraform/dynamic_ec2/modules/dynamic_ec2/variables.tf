0c27c757f6fe354e9abd2a88daeb3fee83418723
dimon7128 <burkov1910@gmail.com>
2024-09-15 11:10:01 +0300

    default variables are added

--- terraform/dynamic_ec2/modules/dynamic_ec2/variables.tf
+++ terraform/dynamic_ec2/modules/dynamic_ec2/variables.tf
@@ -7,16 +7,19 @@ variable "instance_count" {
 variable "ami" {
   description = "AMI to use for the EC2 instance"
   type        = string
+  default = "ami-05134c8ef96964280"
 }
 
 variable "instance_type" {
   description = "Type of EC2 instance to create"
   type        = string
+  default = "t2.micro"
 }
 
 variable "subnet_ids" {
   description = "List of subnet IDs to deploy instances in"
   type        = list(string)
+  default = ["subnet-44da3b3c", "subnet-0d787a46"]
 }
 
 variable "tags" {
---
+++ terraform/dynamic_ec2/terraform.tfvars
@@ -0,0 +1,11 @@
+# # Directly define default values for quick testing or pipeline execution
+# variable "instance_count" { default = 2 }
+# variable "ami" { default = "ami-0c55b159cbfafe1f0" }
+# variable "instance_type" { default = "t2.micro" }
+# variable "subnet_ids" { default = ["subnet-44da3b3c", "subnet-0d787a46"] }
+
+# variable "tags" {
+#   default = {
+#     Environment = "test"
+#   }
+# }
