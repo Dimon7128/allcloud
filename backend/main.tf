37932199ee477336c363ac8a791808fa5e35cbbe
dimon7128 <burkov1910@gmail.com>
2024-09-19 17:42:31 +0300

    not now

---
+++ backend/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "eu-west-1" 
+}
+
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = "my-terraform-state-bucket" 
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.terraform_state_bucket.id
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.terraform_state_lock.id
+}
--- terraform/dynamic_ec2/main.tf
+++ terraform/dynamic_ec2/main.tf
@@ -3,6 +3,24 @@ provider "aws" {
   region = "eu-west-1" 
 }
 
+data "terraform_remote_state" "backend" {
+  backend = "local"
+
+  config = {
+    path = "../backend/state.tfstate" # Path to the first Terraform state
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = data.terraform_remote_state.backend.outputs.s3_bucket_name
+    key            = "terraform/state"
+    region         = "eu-west-1"
+    dynamodb_table = data.terraform_remote_state.backend.outputs.dynamodb_table_name
+    encrypt        = true
+  }
+}
+
 # Module declaration
 module "dynamic_ec2" {
   source        = "./modules/dynamic_ec2"  # Adjust the path as per your directory structure
