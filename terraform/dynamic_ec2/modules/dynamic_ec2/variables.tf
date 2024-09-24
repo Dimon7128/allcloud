4e10d9b6c9134a7a68d5e6a2a9147e1bffcd22ab
dimon7128 <burkov1910@gmail.com>
2024-09-24 12:22:42 +0300

    lets try like this trigger-PR

--- terraform/dynamic_ec2/modules/dynamic_ec2/variables.tf
+++ terraform/dynamic_ec2/modules/dynamic_ec2/variables.tf
@@ -1,29 +1,24 @@
 variable "instance_count" {
   description = "Number of EC2 instances to create"
   type        = number
-  default     = 1
 }
 
 variable "ami" {
   description = "AMI to use for the EC2 instance"
   type        = string
-  default = "ami-03cc8375791cb8bcf"
 }
 
 variable "instance_type" {
   description = "Type of EC2 instance to create"
   type        = string
-  default     = "t2.micro"
 }
 
 variable "subnet_ids" {
   description = "List of subnet IDs to deploy instances in"
   type        = list(string)
-  default     = ["subnet-0c54b26677193b715", "subnet-09a4e1817d45999f8" ]
 }
 
 variable "tags" {
   description = "Tags to apply to resources"
   type        = map(string)
-  default     = {} 
 }
--- terraform/dynamic_ec2/prod/terraform.tfvars
+++ terraform/dynamic_ec2/prod/terraform.tfvars
@@ -1,8 +1,10 @@
-# # Directly define default values for quick testing or pipeline execution
+# Directly define default values for quick testing or pipeline execution
 
-# instance_count = 2
-# ami = "ami-03cc8375791cb8bcf"
-# instance_type = "t2.micro"
-# subnet_ids =  "subnet-0c54b26677193b715valu"
-# tags = "test"
+instance_count = 2
+ami = "ami-03cc8375791cb8bcf"
+instance_type = "t2.micro"
+subnet_ids = ["subnet-0c54b26677193b715valu"]  
+tags = {
+  Name = "test"  
+}
 
